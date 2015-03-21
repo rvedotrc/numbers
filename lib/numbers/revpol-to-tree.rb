678ac29011b185104f570167b1352fc4f8c0ed4f
Rachel Evans <git@rve.org.uk>
2015-03-21 11:37:17 +0000

    revpol-to-tree: return multiple trees

--- lib/numbers/revpol-to-tree.rb
+++ lib/numbers/revpol-to-tree.rb
@@ -12,7 +12,7 @@ module Numbers
         end
       end
 
-      stack.first
+      stack
 
     end
 
--- spec/revpol-to-tree_spec.rb
+++ spec/revpol-to-tree_spec.rb
@@ -5,13 +5,19 @@ describe Numbers::RevpolToTree do
   it "should parse a number" do
     input = '7'
     actual = Numbers::RevpolToTree.parse input
-    expect(actual).to eq(7)
+    expect(actual).to eq([ 7 ])
   end
 
   it "should parse an addition" do
     input = '7 2 +'
     actual = Numbers::RevpolToTree.parse input
-    expect(actual).to eq({ :+ => [ 7, 2 ] })
+    expect(actual).to eq([ { :+ => [ 7, 2 ] } ])
+  end
+
+  it "should should allow multiple trees to be returned" do
+    input = '3 2 + 7'
+    actual = Numbers::RevpolToTree.parse input
+    expect(actual).to eq([ { :+ => [ 3, 2 ] }, 7 ])
   end
 
 end

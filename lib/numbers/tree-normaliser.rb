ba70d54b775f35bde18e7c74ca85c68cab971a69
Rachel Evans <git@rve.org.uk>
2015-03-22 08:17:14 +0000

    Refactor normaliser

--- lib/numbers/tree-normaliser.rb
+++ lib/numbers/tree-normaliser.rb
@@ -8,21 +8,20 @@ module Numbers
       return node if node.kind_of? Fixnum
 
       node.merge(
-        positive: sort_args(node[:positive]),
-        negative: sort_args(node[:negative]),
+        positive: node[:positive].sort {|a,b| compare_operands a, b},
+        negative: node[:negative].sort {|a,b| compare_operands a, b},
       )
     end
 
     private
 
-    def self.sort_args(list)
-      list.sort_by do |c|
-        if c.kind_of? Fixnum
-          c
-        else
-          c[:value]
-        end
-      end.reverse
+    def self.compare_operands(a,b)
+      value_a = a.kind_of?(Fixnum) ? a : a[:value]
+      value_b = b.kind_of?(Fixnum) ? b : b[:value]
+      if value_a != value_b
+        return value_b <=> value_a
+      end
+      0
     end
 
   end
